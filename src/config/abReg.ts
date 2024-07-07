72a87cf166bc5a185fdc1bdc732ad91ea79fbf55
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 14:47:46 -0500

    Update abReg.ts

--- src/config/abReg.ts
+++ src/config/abReg.ts
@@ -1,16 +1,16 @@
-/** @attention 修改正则要注意小括号的位置是否对应，不然还要去修改索引 */
+/** @attention When modifying regex, pay attention to the position of parentheses to ensure they correspond correctly, otherwise index adjustments will be necessary */
 export const ABReg = {
   reg_header:   /^((\s|>\s|-\s|\*\s|\+\s)*)(\[(?!\[)(.*)\])\s*$/,
 
-  // 有前缀版本（给选择器用）
+  // Prefixed version (for selectors)
   reg_headtail: /^((\s|>\s|-\s|\*\s|\+\s)*)(:::)(.*)/,
   reg_list:     /^((\s|>\s|-\s|\*\s|\+\s)*)(-\s|\*\s|\+\s)(.*)/,  //: /^\s*(>\s)*-\s(.*)$/
   reg_code:     /^((\s|>\s|-\s|\*\s|\+\s)*)(```|~~~)(.*)/,      //: /^\s*(>\s|-\s)*(```|~~~)(.*)$/
-  reg_quote:    /^((\s|>\s|-\s|\*\s|\+\s)*)(>\s)(.*)/,          // `- > ` 不匹配，要认为这种是列表
+  reg_quote:    /^((\s|>\s|-\s|\*\s|\+\s)*)(>\s)(.*)/,          // `- > ` does not match; such cases should be recognized as lists
   reg_heading:  /^((\s|>\s|-\s|\*\s|\+\s)*)(\#+\s)(.*)/,
   reg_table:    /^((\s|>\s|-\s|\*\s|\+\s)*)(\|(.*)\|)/,
 
-  // 无前缀版本（给处理器用，处理器不需要处理前缀，前缀在选择器阶段已经被去除了）
+  // Unprefixed version (for processors, which do not need to handle prefixes as they are already removed at the selector stage)
   reg_headtail_noprefix: /^((\s)*)(:::)(.*)/,
   reg_list_noprefix:     /^((\s)*)(-\s|\*\s|\+\s)(.*)/,
   reg_code_noprefix:     /^((\s)*)(```|~~~)(.*)/,      
@@ -18,6 +18,6 @@ export const ABReg = {
   reg_heading_noprefix:  /^((\s)*)(\#+\s)(.*)/,         
   reg_table_noprefix:    /^((\s)*)(\|(.*)\|)/,
 
-  reg_emptyline_noprefix:/^\s*$/,
-  reg_indentline_noprefix:/^\s+?\S/,
-}
+  reg_emptyline_noprefix: /^\s*$/,
+  reg_indentline_noprefix: /^\s+?\S/,
+};
