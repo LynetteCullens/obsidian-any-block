36e05755487654161ea70a435e3a5c6e93c7a9f5
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 15:12:38 -0500

    Update anyblock.d.ts

--- src/@type/anyblock.d.ts
+++ src/@type/anyblock.d.ts
@@ -1,34 +1,36 @@
 // import {registerReplace} from "src/replace/abProcessor"
 
-declare global {
+/*declare global {
   var list_replace: ABReplaceProcessor[];
   var list_option: Map<string,string>;
 
-  // Although regex matching is allowed, a name must be provided to indicate which rule is being used
+  // Although regular expression matching is allowed, a name needs to be provided to indicate which rule is being used
   function registerRepladce(processor: ABReplaceProcessor):void;
 
-  interface ABReplaceProcessor {
-    id: string;
-    name: string;
-    is_render: boolean;
-    (el: HTMLDivElement, header: string, content: string): HTMLElement | null;
+  interface ABReplaceProcessor{
+    id: string
+    name: string
+    is_render: boolean
+    (el:HTMLDivElement, header:string, content:string): HTMLElement|null
   }
-
-  /// This TypeScript decorator. If it cannot be used, configure it accordingly
-  /// Modify list_replace and list_option through generators
+  /// This is TS decorator. If it cannot be used, configure it
+  /// The generator needs to modify list_replace and list_option
   function register_abReplace(): register_abReplace2;
-  interface register_abReplace2 {
-    (target: ABReplaceProcessor3): void;
+  interface register_abReplace2{
+    function (target: ABReplaceProcessor3): void
   }
-  interface ABReplaceProcessor3 {
-    id: string;
-    name: string;
-    is_render: boolean;
-    process(el: HTMLDivElement, header: string, content: string): HTMLElement | null;
+  interface ABReplaceProcessor3{
+    id: string
+    name: string
+    is_render: boolean
+    process (el:HTMLDivElement, header:string, content:string): HTMLElement|null
   }
-  type ABReplaceProcessor2 = (el: HTMLDivElement, header: string, content: string) => HTMLElement | null;
-}
+  type ABReplaceProcessor2 = {
+    (el:HTMLDivElement, header:string, content:string): HTMLElement|null
+  }
+}*/
 
-// Add recommended code selectors
 
-// Add recommended list selectors
+// Add code selector recommendations
+
+// Add list selector recommendations
