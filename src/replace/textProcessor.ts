52572160ace8967649a4771ca8c3dc19da066481
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 16:22:35 -0500

    Update textProcessor.ts

--- src/replace/textProcessor.ts
+++ src/replace/textProcessor.ts
@@ -14,9 +14,9 @@ export const mermaid_init = async () => {
 };
 
 /**
- * 将registerABProcessor的调用分成两步是因为：
- * 1. 能方便在大纲里快速找到想要的处理器
- * 2. 让处理器能互相调用
+ * Splitting the call of registerABProcessor into two steps because:
+ * 1. It's easier to quickly find the desired processor in the outline
+ * 2. Allows processors to call each other
  */
 
 const process_md:ABProcessorSpecSimp = {
@@ -37,7 +37,7 @@ ABProcessManager.getInstance().registerABProcessor(process_md)
 
 const process_quote:ABProcessorSpecSimp = {
   id: "quote",
-  name: "增加引用块",
+  name: "Add Quote Block",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -48,10 +48,10 @@ ABProcessManager.getInstance().registerABProcessor(process_quote)
 
 const process_code:ABProcessorSpecSimp = {
   id: "code",
-  name: "增加代码块",
+  name: "Add Code Block",
   match: /^code(\((.*)\))?$/,
   default: "code()",
-  detail: "不加`()`表示用原文本的第一行作为代码类型，括号类型为空表示代码类型为空",
+  detail: "Without `()` indicates using the first line of the original text as the code type, empty parentheses indicate that the code type is empty",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -65,7 +65,7 @@ ABProcessManager.getInstance().registerABProcessor(process_code)
 
 const process_Xquote:ABProcessorSpecSimp = {
   id: "Xquote",
-  name: "去除引用块",
+  name: "Remove Quote Block",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -78,10 +78,10 @@ ABProcessManager.getInstance().registerABProcessor(process_Xquote)
 
 const process_Xcode:ABProcessorSpecSimp = {
   id: "Xcode",
-  name: "去除代码块",
+  name: "Remove Code Block",
   match: /^Xcode(\((true|false)\))?$/,
   default: "Xcode(true)",
-  detail: "参数为是否移除代码类型, 默认为false。记法: code|Xcode或code()|Xcode(true)内容不变",
+  detail: "The parameter is whether to remove the code type, defaults to false. Notation: code|Xcode or code()|Xcode(true) content remains unchanged",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -91,26 +91,26 @@ const process_Xcode:ABProcessorSpecSimp = {
     if (matchs[1]=="") remove_flag=false
     else remove_flag= (matchs[2]=="true")
     let list_content = content.split("\n")
-    // 开始去除
+    // Start removing
     let code_flag = ""
     let line_start = -1
     let line_end = -1
     for (let i=0; i<list_content.length; i++){
-      if (code_flag==""){     // 寻找开始标志
+      if (code_flag==""){     // Look for the start flag
         const match_tmp = list_content[i].match(ABReg.reg_code)
         if(match_tmp){
           code_flag = match_tmp[3]
           line_start = i
         }
       }
-      else {                  // 寻找结束标志
+      else {                  // Look for the end flag
         if(list_content[i].indexOf(code_flag)>=0){
           line_end = i
           break
         }
       }
     }
-    if(line_start>=0 && line_end>0) { // 避免有头无尾的情况
+    if(line_start>=0 && line_end>0) { // Avoid situations where there is a beginning but no end
       if(remove_flag) list_content[line_start] = list_content[line_start].replace(/^```(.*)$|^~~~(.*)$/, "")
       else list_content[line_start] = list_content[line_start].replace(/^```|^~~~/, "")
       list_content[line_end] = list_content[line_end].replace(/^```|^~~~/, "")
@@ -123,7 +123,7 @@ ABProcessManager.getInstance().registerABProcessor(process_Xcode)
 
 const process_X:ABProcessorSpecSimp = {
   id: "X",
-  name: "去除代码或引用块",
+  name: "Remove Code or Quote Block",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -141,7 +141,7 @@ ABProcessManager.getInstance().registerABProcessor(process_X)
 
 const process_code2quote:ABProcessorSpecSimp = {
   id: "code2quote",
-  name: "代码转引用块",
+  name: "Code to Quote Block",
   process_alias: "Xcode|quote",
   process: ()=>{}
 }
@@ -149,7 +149,7 @@ ABProcessManager.getInstance().registerABProcessor(process_code2quote)
 
 const process_quote2code:ABProcessorSpecSimp = {
   id: "quote2code",
-  name: "引用转代码块",
+  name: "Quote to Code Block",
   match: /^quote2code(\((.*)\))?$/,
   default: "quote2code()",
   process_alias: "Xquote|code%1",
@@ -166,23 +166,23 @@ ABProcessManager.getInstance().registerABProcessor(process_quote2code)
 
 const process_slice:ABProcessorSpecSimp = {
   id: "slice",
-  name: "切片",
+  name: "Slice",
   match: /^slice\((\s*\d+\s*?)(,\s*-?\d+\s*)?\)$/,
-  detail: "和js的slice方法是一样的",
+  detail: "Same as the js slice method",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
-    // slice好像不怕溢出或交错，会自动变空数组。就很省心，不用判断太多的东西
+    // Slice seems to be unafraid of overflow or crossover, it will automatically become an empty array. It's very convenient and doesn't need to judge too many things.
     const list_match = header.match(/^slice\((\s*\d+\s*)(,\s*-?\d+\s*)?\)$/)
     if (!list_match) return content
     const arg1 = Number(list_match[1].trim())
     if (isNaN(arg1)) return content
     const arg2 = Number(list_match[2].replace(",","").trim())
-    // 单参数
+    // Single parameter
     if (isNaN(arg2)) {
       return content.split("\n").slice(arg1).join("\n")
     }
-    // 双参数
+    // Double parameter
     else {
       return content.split("\n").slice(arg1, arg2).join("\n")
     }
@@ -192,9 +192,9 @@ ABProcessManager.getInstance().registerABProcessor(process_slice)
 
 const process_add:ABProcessorSpecSimp = {
   id: "add",
-  name: "增添内容",
+  name: "Add Content",
   match: /^add\((.*?)(,\s*-?\d+\s*)?\)$/,
-  detail: "增添. 参数2为行序, 默认0, 行尾-1。会插行增添",
+  detail: "Add. Parameter 2 is the line order, default 0, line end -1. It will be inserted and added",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -224,10 +224,10 @@ ABProcessManager.getInstance().registerABProcessor(process_add)
 
 const process_title2list:ABProcessorSpecSimp = {
   id: "title2list",
-  name: "标题到列表",
+  name: "Title to List",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
-  detail: "也可以当作是更强大的列表解析器",
+  detail: "Can also be used as a more powerful list parser",
   process: (el, header, content)=>{
     content = ListProcess.title2list(content, el)
     return content
@@ -237,7 +237,7 @@ ABProcessManager.getInstance().registerABProcessor(process_title2list)
 
 const process_title2table:ABProcessorSpecSimp = {
   id: "title2table",
-  name: "标题到表格",
+  name: "Title to Table",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
@@ -250,7 +250,7 @@ ABProcessManager.getInstance().registerABProcessor(process_title2table)
 
 const process_title2mindmap:ABProcessorSpecSimp = {
   id: "title2mindmap",
-  name: "标题到脑图",
+  name: "Title to Mind Map",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
@@ -263,7 +263,7 @@ ABProcessManager.getInstance().registerABProcessor(process_title2mindmap)
 
 const process_listroot:ABProcessorSpecSimp = {
   id: "listroot",
-  name: "增加列表根",
+  name: "Add List Root",
   match: /^listroot\((.*)\)$/,
   default: "listroot(root)",
   process_param: ProcessDataType.text,
@@ -281,7 +281,7 @@ ABProcessManager.getInstance().registerABProcessor(process_listroot)
 
 const process_listXinline:ABProcessorSpecSimp = {
   id: "listXinline",
-  name: "列表消除内联换行",
+  name: "Remove Inline Line Breaks from List",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -292,7 +292,7 @@ ABProcessManager.getInstance().registerABProcessor(process_listXinline)
 
 const process_list2table:ABProcessorSpecSimp = {
   id: "list2table",
-  name: "列表转表格",
+  name: "List to Table",
   match: /list2(md)?table(T)?/,
   default: "list2table",
   process_param: ProcessDataType.text,
@@ -308,7 +308,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2table)
 
 const process_list2lt:ABProcessorSpecSimp = {
   id: "list2lt",
-  name: "列表转列表表格",
+  name: "List to List Table",
   match: /list2(md)?lt(T)?/,
   default: "list2lt",
   process_param: ProcessDataType.text,
@@ -324,7 +324,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2lt)
 
 const process_list2folder:ABProcessorSpecSimp = {
   id: "list2folder",
-  name: "列表转树状目录",
+  name: "List to Tree Directory",
   match: /list2(md)?folder(T)?/,
   default: "list2folder",
   process_param: ProcessDataType.text,
@@ -340,7 +340,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2lt)
 
 const process_list2ut:ABProcessorSpecSimp = {
   id: "list2ut",
-  name: "列表转二维表格",
+  name: "List to Two-Dimensional Table",
   match: /list2(md)?ut(T)?/,
   default: "list2ut",
   process_param: ProcessDataType.text,
@@ -356,7 +356,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2ut)
 
 const process_list2timeline:ABProcessorSpecSimp = {
   id: "list2timeline",
-  name: "一级列表转时间线",
+  name: "Level 1 List to Timeline",
   match: /list2(md)?timeline(T)?/,
   default: "list2mdtimeline",
   process_param: ProcessDataType.text,
@@ -372,7 +372,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2timeline)
 
 const process_list2tab:ABProcessorSpecSimp = {
   id: "list2tab",
-  name: "一级列表转标签栏",
+  name: "Level 1 List to Tab Bar",
   match: /list2(md)?tab(T)?$/,
   default: "list2mdtab",
   process_param: ProcessDataType.text,
@@ -388,7 +388,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2tab)
 
 const process_list2mermaid:ABProcessorSpecSimp = {
   id: "list2mermaid",
-  name: "列表转mermaid流程图",
+  name: "List to Mermaid Flowchart",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
@@ -400,7 +400,7 @@ ABProcessManager.getInstance().registerABProcessor(process_list2mermaid)
 
 const process_list2mindmap:ABProcessorSpecSimp = {
   id: "list2mindmap",
-  name: "列表转mermaid思维导图",
+  name: "List to Mermaid Mind Map",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
@@ -412,10 +412,10 @@ ABProcessManager.getInstance().registerABProcessor(process_list2mindmap)
 
 const process_callout:ABProcessorSpecSimp = {
   id: "callout",
-  name: "callout语法糖",
+  name: "Callout Syntax Sugar",
   match: /^\!/,
   default: "!note",
-  detail: "需要obsidian 0.14版本以上来支持callout语法",
+  detail: "Requires obsidian 0.14 or above to support callout syntax",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.text,
   process: (el, header, content)=>{
@@ -426,10 +426,10 @@ ABProcessManager.getInstance().registerABProcessor(process_callout)
 
 const process_mermaid:ABProcessorSpecSimp = {
   id: "mermaid",
-  name: "新mermaid",
+  name: "New Mermaid",
   match: /^mermaid(\((.*)\))?$/,
   default: "mermaid(graph TB)",
-  detail: "由于需要兼容脑图，这里会使用插件内置的最新版mermaid",
+  detail: "Due to the need to be compatible with mindmaps, the latest version of mermaid built into the plugin will be used here",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
@@ -450,14 +450,14 @@ ABProcessManager.getInstance().registerABProcessor(process_mermaid)
 
 const process_text:ABProcessorSpecSimp = {
   id: "text",
-  name: "纯文本",
-  detail: "其实一般会更推荐用code()代替，那个更精确",
+  name: "Plain Text",
+  detail: "Actually, it's generally more recommended to use code() instead, which is more precise",
   process_param: ProcessDataType.text,
   process_return: ProcessDataType.el,
   process: (el, header, content)=>{
-    // 文本元素。pre不好用，这里还是得用<br>换行最好
+    // Text element. Pre is not good to use, here we still have to use <br> to break lines
     // `<p>${content.split("\n").map(line=>{return "<span>"+line+"</span>"}).join("<br/>")}</p>`
-    el.innerHTML = `<p>${content.replace(/ /g, "&nbsp;").split("\n").join("<br/>")}</p>`
+    el.innerHTML = `<p>${content.replace(/ /g, " ").split("\n").join("<br/>")}</p>`
     return el
   }
 }
