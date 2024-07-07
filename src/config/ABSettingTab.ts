9dd57b3eced1adedcc248c1315eab12a33101289
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 14:46:54 -0500

    Update ABSettingTab.ts

--- src/config/ABSettingTab.ts
+++ src/config/ABSettingTab.ts
@@ -1,41 +1,43 @@
-import {App, PluginSettingTab, Setting, Modal} from "obsidian"
-import type AnyBlockPlugin from "../main"
-import {ABProcessManager} from "src/replace/abProcessorManager"
-import type {ABProcessorSpecUser} from "src/replace/abProcessorInterface"
-import {} from "src/replace/textProcessor"    // 加载所有处理器和选择器
-import {} from "src/replace/listProcessor"    // ^
-import {} from "src/replace/decoProcessor"    // ^
-import {} from "src/replace/exProcessor"      // ^
-import {} from "src/manager/abMdBaseSelector" // ^
-import {generateSelectorInfoTable} from "src/manager/abMdSelector"  // ^
+import { App, PluginSettingTab, Setting, Modal } from "obsidian";
+import type AnyBlockPlugin from "../main";
+import { ABProcessManager } from "src/replace/abProcessorManager";
+import type { ABProcessorSpecUser } from "src/replace/abProcessorInterface";
+import {} from "src/replace/textProcessor"; // Load all processors and selectors
+import {} from "src/replace/listProcessor"; // ^
+import {} from "src/replace/decoProcessor"; // ^
+import {} from "src/replace/exProcessor"; // ^
+import {} from "src/manager/abMdBaseSelector"; // ^
+import { generateSelectorInfoTable } from "src/manager/abMdSelector"; // ^
 
-/** 设置值接口 */
+/** Setting Interface */
 export interface ABSettingInterface {
-  select_list: ConfSelect
-  select_quote: ConfSelect
-  select_code: ConfSelect
-  select_heading: ConfSelect
-  select_brace: ConfSelect
+  select_list: ConfSelect;
+  select_quote: ConfSelect;
+  select_code: ConfSelect;
+  select_heading: ConfSelect;
+  select_brace: ConfSelect;
   //is_range_html: boolean
   //is_range_brace: boolean
-  decoration_source: ConfDecoration
-  decoration_live: ConfDecoration
-  decoration_render: ConfDecoration
-  is_neg_level: boolean,
-  user_processor: ABProcessorSpecUser[]
-}
-export enum ConfSelect{
-  no = "no",
-  ifhead = "ifhead",
-  yes = "yes"
-}
-export enum ConfDecoration{
-  none = "none",
-  inline = "inline",
-  block = "block"
+  decoration_source: ConfDecoration;
+  decoration_live: ConfDecoration;
+  decoration_render: ConfDecoration;
+  is_neg_level: boolean;
+  user_processor: ABProcessorSpecUser[];
 }
 
-/** 设置值默认项 */
+export enum ConfSelect {
+  no = "no",
+  ifhead = "ifhead",
+  yes = "yes",
+}
+
+export enum ConfDecoration {
+  none = "none",
+  inline = "inline",
+  block = "block",
+}
+
+/** Default Settings */
 export const AB_SETTINGS: ABSettingInterface = {
   select_list: ConfSelect.ifhead,
   select_quote: ConfSelect.ifhead,
@@ -46,174 +48,134 @@ export const AB_SETTINGS: ABSettingInterface = {
   decoration_live: ConfDecoration.block,
   decoration_render: ConfDecoration.block,
   is_neg_level: false,
-  user_processor: []
-}
+  user_processor: [],
+};
 
-/** 设置值面板 */
+/** Settings Tab */
 export class ABSettingTab extends PluginSettingTab {
-	plugin: AnyBlockPlugin
-  processorPanel: HTMLElement
-  selectorPanel: HTMLElement
+  plugin: AnyBlockPlugin;
+  processorPanel: HTMLElement;
+  selectorPanel: HTMLElement;
 
-	constructor(app: App, plugin: AnyBlockPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-    for (let item of plugin.settings.user_processor){
-      ABProcessManager.getInstance().registerABProcessor(item)
+  constructor(app: App, plugin: AnyBlockPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+    for (let item of plugin.settings.user_processor) {
+      ABProcessManager.getInstance().registerABProcessor(item);
     }
-	}
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
     containerEl.empty();
-    let settings = this.plugin.settings
+    let settings = this.plugin.settings;
 
-		containerEl.createEl('h2', {text: '范围管理器'});
+    containerEl.createEl("h2", { text: "Range Manager" });
 
-    this.selectorPanel = generateSelectorInfoTable(containerEl)
+    this.selectorPanel = generateSelectorInfoTable(containerEl);
 
-    /*containerEl.createEl('h2', {text: '装饰管理器'});
-
-    new Setting(containerEl)
-      .setName('源码模式中启用')
-      .setDesc('推荐：不启用')
-			.addDropdown((component)=>{
-        component
-        .addOption(ConfDecoration.none, "不启用")
-        .addOption(ConfDecoration.inline, "仅启用线装饰")
-        .addOption(ConfDecoration.block, "启用块装饰")
-        .setValue(settings.decoration_source)
-        .onChange(async v=>{
-          // @ts-ignore 这里枚举必然包含v值的
-          settings.decoration_source = ConfDecoration[v]  
-          await this.plugin.saveSettings();    
-        })
-      })
+    containerEl.createEl("h2", { text: "View All Registered Commands" });
 
     new Setting(containerEl)
-      .setName('实时模式中启用')
-      .setDesc('推荐：启用块装饰/线装饰')
-			.addDropdown((component)=>{
+      .setName("Add New Registered Command")
+      .setDesc(
+        "@todo: To delete or modify after adding, please open the data.json folder"
+      )
+      .addButton((component) => {
         component
-        .addOption(ConfDecoration.none, "不启用")
-        .addOption(ConfDecoration.inline, "仅启用线装饰")
-        .addOption(ConfDecoration.block, "启用块装饰")
-        .setValue(settings.decoration_live)
-        .onChange(async v=>{
-          // @ts-ignore 这里枚举必然包含v值的
-          settings.decoration_live = ConfDecoration[v]
-          await this.plugin.saveSettings(); 
-        })
-      })
+          .setIcon("plus-circle")
+          .onClick((e) => {
+            new ABProcessorModal(this.app, async (result) => {
+              ABProcessManager.getInstance().registerABProcessor(result);
+              settings.user_processor.push(result);
+              await this.plugin.saveSettings();
+              this.processorPanel.remove();
+              this.processorPanel =
+                ABProcessManager.getInstance().generateProcessorInfoTable(
+                  containerEl
+                );
+            }).open();
+          });
+      });
 
-    new Setting(containerEl)
-      .setName('渲染模式中启用')
-      .setDesc('推荐：启用块装饰')
-			.addDropdown((component)=>{
-        component
-        .addOption(ConfDecoration.none, "不启用")
-        .addOption(ConfDecoration.block, "启用块装饰")
-        .setValue(settings.decoration_render)
-        .onChange(async v=>{
-          // @ts-ignore 这里枚举必然包含v值的
-          settings.decoration_render = ConfDecoration[v]    
-          await this.plugin.saveSettings(); 
-        })
-      })*/
-
-    containerEl.createEl('h2', {text: '查看所有注册指令'});
-
-    new Setting(containerEl)
-      .setName('添加新的注册指令')
-      .setDesc('@todo: 添加后要删除或修改请打开data.json文件夹')
-      .addButton(component => {
-        component
-        .setIcon("plus-circle")
-        .onClick(e => {
-          new ABProcessorModal(this.app, async (result)=>{
-            ABProcessManager.getInstance().registerABProcessor(result)
-            settings.user_processor.push(result)
-            await this.plugin.saveSettings();
-            this.processorPanel.remove()
-            this.processorPanel = ABProcessManager.getInstance().generateProcessorInfoTable(containerEl)
-          }).open()
-        })
-      })
-    this.processorPanel = ABProcessManager.getInstance().generateProcessorInfoTable(containerEl)
-	}
+    this.processorPanel =
+      ABProcessManager.getInstance().generateProcessorInfoTable(containerEl);
+  }
 }
 
 class ABProcessorModal extends Modal {
-  args: ABProcessorSpecUser
-  onSubmit: (args: ABProcessorSpecUser)=>void
+  args: ABProcessorSpecUser;
+  onSubmit: (args: ABProcessorSpecUser) => void;
 
-  constructor(
-    app: App, 
-    onSubmit: (args: ABProcessorSpecUser)=>void
-  ) {
+  constructor(app: App, onSubmit: (args: ABProcessorSpecUser) => void) {
     super(app);
     this.args = {
       id: "",
       name: "",
       match: "",
-      process_alias: ""
-    }
-    this.onSubmit = onSubmit
+      process_alias: "",
+    };
+    this.onSubmit = onSubmit;
   }
 
-  onOpen() {	// onOpen() 方法在对话框打开时被调用，它负责创建对话框中的内容。想要获取更多信息，可以查阅 HTML elements。
+  onOpen() {
     let { contentEl } = this;
-    contentEl.setText("自定义处理器");
-    new Setting(contentEl)
-      .setName("处理器唯一id")
-      .setDesc("不与其他处理器冲突即可")
-      .addText((text)=>{
-        text.onChange((value) => {
-          this.args.id = value
-        })
-      })
+    contentEl.setText("Custom Processor");
 
     new Setting(contentEl)
-      .setName("注册器名字")
-      .addText((text)=>{
+      .setName("Processor Unique ID")
+      .setDesc("Must not conflict with other processors")
+      .addText((text) => {
         text.onChange((value) => {
-        this.args.name = value
-      })
-    })
+          this.args.id = value;
+        });
+      });
 
     new Setting(contentEl)
-      .setName("注册器匹配名")
-      .setDesc("用/包括起来则表示正则")
-      .addText((text)=>{
+      .setName("Processor Name")
+      .addText((text) => {
         text.onChange((value) => {
-        this.args.match = value
-      })
-    })
+          this.args.name = value;
+        });
+      });
 
     new Setting(contentEl)
-      .setName("注册器替换为")
-      .setDesc("用/包括起来则判断为正则")
-      .addText((text)=>{
+      .setName("Processor Matching Name")
+      .setDesc("Use / to denote regex")
+      .addText((text) => {
         text.onChange((value) => {
-        this.args.process_alias = value
-      })
-    })
+          this.args.match = value;
+        });
+      });
 
     new Setting(contentEl)
-      .addButton(btn => {
-        btn
-        .setButtonText("提交")
-        .setCta() // 这个不知道什么意思
+      .setName("Processor Replacement")
+      .setDesc("Use / to denote regex")
+      .addText((text) => {
+        text.onChange((value) => {
+          this.args.process_alias = value;
+        });
+      });
+
+    new Setting(contentEl).addButton((btn) => {
+      btn
+        .setButtonText("Submit")
+        .setCta()
         .onClick(() => {
-          if(this.args.id && this.args.name && this.args.match && this.args.process_alias){
+          if (
+            this.args.id &&
+            this.args.name &&
+            this.args.match &&
+            this.args.process_alias
+          ) {
             this.close();
             this.onSubmit(this.args);
           }
-        })
-      })
+        });
+    });
   }
 
-  onClose() {	// onClose() 方法在对话框被关闭时调用，它负责清理对话框所占用的资源。
+  onClose() {
     let { contentEl } = this;
     contentEl.empty();
   }
