a1f7acc7ed84cc2c46c2be2751489f3587fc9ce3
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 16:24:35 -0500

    Update GeneratorBranchTable.svelte

--- src/svelte/GeneratorBranchTable.svelte
+++ src/svelte/GeneratorBranchTable.svelte
@@ -9,19 +9,19 @@
 
   let table:HTMLDivElement;
   onMount(async()=>{
-    // 表格数据 组装成表格
+    // Tabular data assembled into tables
     table.addClasses(["ab-table", "ab-branch-table"])
     if (modeT) table.setAttribute("modeT", "true")
     let thead
-    if(list_tableInfo[0].content.indexOf("< ")==0){ // 判断是否有表头
+    if(list_tableInfo[0].content.indexOf("< ")==0){ // Determine whether there is a header
       thead = table.createEl("thead")
       list_tableInfo[0].content=list_tableInfo[0].content.replace(/^\<\s/,"")
     }
     const tbody = table.createEl("tbody")
-    for (let index_line=0; index_line<prev_line+1; index_line++){ // 遍历表格行，创建tr……
+    for (let index_line=0; index_line<prev_line+1; index_line++){ // Iterate over table rows，创建tr……
       let is_head
       let tr
-      if (index_line==0 && thead){ // 判断是否第一行&&是否有表头
+      if (index_line==0 && thead){ // Determine whether the first row && has a header
         tr = thead.createEl("tr")
         is_head = true
       }
