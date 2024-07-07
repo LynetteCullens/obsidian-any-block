f246a766a770af4bc7da1f236a2c97bbade95a3b
LynetteCullens <45722611+LynetteCullens@users.noreply.github.com>
2024-07-07 15:31:17 -0500

    Update abCodeblockManager.ts

--- src/manager3/abCodeblockManager.ts
+++ src/manager3/abCodeblockManager.ts
@@ -1,14 +1,20 @@
-import {
+import type {MarkdownPostProcessorContext} from "obsidian"
+import{
   MarkdownRenderChild,
   MarkdownRenderer,
 } from "obsidian";
 
-export class ABCodeblockManager {
-  static processor(src, blockEl, ctx) {
+export class ABCodeblockManager{
+  static processor(
+    // plugin_this: AnyBlockPlugin,                       // 使用bind方法被绑进来的
+    src: string,                                // Code block content
+    blockEl: HTMLElement,                       // Element where the code block is rendered
+    ctx: MarkdownPostProcessorContext,
+  ) {
     let child = new MarkdownRenderChild(blockEl);
     ctx.addChild(child);
-
-    blockEl.classList.add("markdown-rendered");
+  
+    blockEl.addClass("markdown-rendered")
     MarkdownRenderer.renderMarkdown(src, blockEl, ctx.sourcePath, child);
   }
 }
